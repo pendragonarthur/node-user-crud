abd01bbdeea00af02ac8c5a966eebb1ac7c3e1f4
tuzinmaibrabo <arthurquarxma@outlook.com>
2023-09-05 15:45:32 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+import mongoose from "mongoose";
+import router from "./router";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log(`Server running on port http://localhost:8080/`);
+});
+
+const MONGO_URL =
+  "mongodb+srv://arthur:3Fosfato@cluster0.qnvaww2.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => {
+  console.log(error);
+});
+
+app.use("/", router());
